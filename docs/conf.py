9a647a544943fdf8a8444eb86c57c975b3b2cabe
Katherine Rosenfeld <krosenf@gmail.com>
2026-03-02 10:52:03 -0800

    🔖 Bump version: 0.9.1 → 0.9.2

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ project = "laser-measles"
 copyright = f"2024 - {sc.now().year}, Bill & Melinda Gates Foundation. All rights reserved."
 
 # The short X.Y version
-version = release = "0.9.1"
+version = release = "0.9.2"
 
 
 # -- General configuration ---------------------------------------------------
--- src/laser/measles/__init__.py
+++ src/laser/measles/__init__.py
@@ -6,7 +6,7 @@ using agent-based and compartmental models with various spatial and temporal con
 """
 
 # ruff: noqa: F401, F403, E402
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 # --- Exports ---
 MEASLES_MODULES = ["laser.measles.abm", "laser.measles.compartmental", "laser.measles.biweekly"]
